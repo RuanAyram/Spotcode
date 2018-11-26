2f4dafbbf613ce87504ba8da65b12863dabbd327
Ruan <ruan_kaylo@yahoo.com.br>
2018-11-25 22:30:02 -0300

    Aula 2 - OneBitCode

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
     devise_for :users
 
-    root to: "home#index"
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    authenticated :user do
+        root to: "dashboard#index", as: :authenticated_root
+        resources :search, only: [:index, :new], as: :searches
+        resources :categories, only: :show
+        resources :artists, only: :show
+        resources :albums, only: :show
+    end
+
+    unauthenticated :user do
+        root to: "home#index"
+    end
 end
